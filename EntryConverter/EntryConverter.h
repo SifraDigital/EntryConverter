20e4ab4cbbc46d2736dbb27f7fb700bc6ab6fc0e
chaimgross <chaimgross1026@gmail.com>
2020-08-24 22:17:25 +0300

    Initial Commit

---
+++ EntryConverter/EntryConverter.h
@@ -0,0 +1,19 @@
+//
+//  EntryConverter.h
+//  EntryConverter
+//
+//  Created by Chaim Gross on 24/08/2020.
+//  Copyright © 2020 Chaim Gross. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EntryConverter.
+FOUNDATION_EXPORT double EntryConverterVersionNumber;
+
+//! Project version string for EntryConverter.
+FOUNDATION_EXPORT const unsigned char EntryConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EntryConverter/PublicHeader.h>
+
+
